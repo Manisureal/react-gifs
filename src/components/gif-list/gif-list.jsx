3a3b9fa1d035eafa6dbaf5e81258cdac72d72c57
manisureal <mansoor.zaman@sage.com>
2019-07-23 16:14:47 +0100

    simplified Gif and GifList Components

--- src/components/gif-list/gif-list.jsx
+++ src/components/gif-list/gif-list.jsx
@@ -1,29 +1,23 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import Gif from '.././gif/gif.jsx';
 
 import "./gif-list.scss";
 
 class GifList extends Component {
 	
 	displayGifs() {
-		let url;
 		if (this.props.gifs != null ){
-			this.props.gifs.forEach( (gif) => {
-				url = `https://media.giphy.com/media/${gif.id}/giphy.gif`;
-				console.log(gif.id);
-				<img src={url} />;
+			return this.props.gifs.map( (gif) => {
+				return <Gif gif={gif.id} />
 			})
 		}
 		return null
 	}
 
 	render(){
-		console.log(this.props.gifs)
-		// let url = `https://media.giphy.com/media/3o6fJ8bYYnCW95ST0A/giphy.gif`
-
-		// let url = `api.giphy.com/v1/gifs/${this.props.gifs[0].id}` ? this.props.gifs : null
 		return(
-			<div>
+			<div className="gif-list">
 				{ this.displayGifs() }
 			</div>
 		)
--- src/components/gif/gif.jsx
+++ src/components/gif/gif.jsx
@@ -5,9 +5,14 @@ import "./gif.scss";
 
 class Gif extends Component {
 	render(){
-		return(
-			<img src="https://images.unsplash.com/photo-1563141985-414bd4bd5c82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80" height="500px" width="750px"/>
-		)
+		let url = `https://media.giphy.com/media/${this.props.gif}/giphy.gif`;
+		let img = <img src={url} />
+
+		return img
+			// <>
+				 
+			// </>
+		
 	}
 
 }
