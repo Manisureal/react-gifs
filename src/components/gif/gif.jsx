b1b897fd1bafcde017ae95992b1fc92d21d488af
manisureal <mansoor.zaman@sage.com>
2019-07-24 16:25:50 +0100

    created handleClick Fc and improved render Fc

--- src/components/gif/gif.jsx
+++ src/components/gif/gif.jsx
@@ -4,15 +4,23 @@ import ReactDom from 'react-dom';
 import "./gif.scss";
 
 class Gif extends Component {
+	handleClick = (e) => {
+		this.props.callBackFromGif(e.currentTarget.src)
+	}
+
 	render(){
 		let url = `https://media.giphy.com/media/${this.props.gif}/giphy.gif`;
-		let img = <img src={url} />
+		let img = <img src={url} onClick={this.handleClick}/>;
 
-		return img
-			// <>
-				 
-			// </>
-		
+		if (this.props.gif != undefined){
+			return img;
+		}
+
+		if (this.props.displaySelected != null){
+			return <img src={this.props.displaySelected} />;
+		}
+
+		return null;
 	}
 
 }
